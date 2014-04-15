90aefa7fa4ec26deee45122e0f46b9105f7f0e80
James Ottaway <git@james.ottaway.io>
2014-04-15 15:20:17 +1000

    Clone the Xamarin platforms to make a Mono one

---
+++ src/SQLite.Net.Platform.Mono/SQLitePlatformMono.cs
@@ -0,0 +1,20 @@
+﻿using SQLite.Net.Interop;
+
+namespace SQLite.Net.Platform.Mono
+{
+    public class SQLitePlatformMono : ISQLitePlatform
+    {
+        public SQLitePlatformMono()
+        {
+            SQLiteApi = new SQLiteApiMono();
+            StopwatchFactory = new StopwatchFactoryMono();
+            ReflectionService = new ReflectionServiceMono();
+            VolatileService = new VolatileServiceMono();
+        }
+
+        public ISQLiteApi SQLiteApi { get; private set; }
+        public IStopwatchFactory StopwatchFactory { get; private set; }
+        public IReflectionService ReflectionService { get; private set; }
+        public IVolatileService VolatileService { get; private set; }
+    }
+}
