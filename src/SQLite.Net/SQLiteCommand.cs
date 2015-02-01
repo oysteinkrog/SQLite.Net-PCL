1979285a81b2c57616b214d21dd4ffca7a80ffd7
ravensorb <sanderson@eye-catcher.com>
2015-02-01 10:13:09 -0500

    Added Comments to new ContractResovler class and interface Changed calls over to the new "CreateObject" method that is now part of the ContractResolver.

--- src/SQLite.Net/SQLiteCommand.cs
+++ src/SQLite.Net/SQLiteCommand.cs
@@ -123,7 +123,7 @@ namespace SQLite.Net
 
                 while (_sqlitePlatform.SQLiteApi.Step(stmt) == Result.Row)
                 {
-					object obj = this._conn.Resolver.Create(map.MappedType, null);
+					object obj = this._conn.Resolver.CreateObject(map.MappedType);
                     //object obj = Activator.CreateInstance(map.MappedType);
                     for (int i = 0; i < cols.Length; i++)
                     {
@@ -401,7 +401,7 @@ namespace SQLite.Net
             if (interfaces.Contains(typeof(ISerializable<string>)))
             {
                 var value = _sqlitePlatform.SQLiteApi.ColumnText16(stmt, index);
-	            return this._conn.Resolver.Create(clrType, new object[] { value });
+	            return this._conn.Resolver.CreateObject(clrType, new object[] { value });
                 //return Activator.CreateInstance(clrType, new object[]{ value });
             }
             if (clrType == typeof(Int32))
@@ -411,7 +411,7 @@ namespace SQLite.Net
             if (interfaces.Contains(typeof(ISerializable<Int32>)))
             {
                 var value = _sqlitePlatform.SQLiteApi.ColumnInt(stmt, index);
-				return this._conn.Resolver.Create(clrType, new object[] { value });
+				return this._conn.Resolver.CreateObject(clrType, new object[] { value });
 				//return Activator.CreateInstance(clrType, new object[]{ value });
             }
             if (clrType == typeof(Boolean))
@@ -421,7 +421,7 @@ namespace SQLite.Net
             if (interfaces.Contains(typeof(ISerializable<Boolean>)))
             {
                 var value = _sqlitePlatform.SQLiteApi.ColumnInt(stmt, index) == 1;
-				return this._conn.Resolver.Create(clrType, new object[] { value });
+				return this._conn.Resolver.CreateObject(clrType, new object[] { value });
 				//return Activator.CreateInstance(clrType, new object[]{ value });
             }
             if (clrType == typeof(double))
@@ -431,7 +431,7 @@ namespace SQLite.Net
             if (interfaces.Contains(typeof(ISerializable<double>)))
             {
                 var value = _sqlitePlatform.SQLiteApi.ColumnDouble(stmt, index);
-				return this._conn.Resolver.Create(clrType, new object[] { value });
+				return this._conn.Resolver.CreateObject(clrType, new object[] { value });
 				//return Activator.CreateInstance(clrType, new object[]{ value });
             }
             if (clrType == typeof(float))
@@ -441,7 +441,7 @@ namespace SQLite.Net
             if (interfaces.Contains(typeof(ISerializable<float>)))
             {
                 var value = (float)_sqlitePlatform.SQLiteApi.ColumnDouble(stmt, index);
-				return this._conn.Resolver.Create(clrType, new object[] { value });
+				return this._conn.Resolver.CreateObject(clrType, new object[] { value });
 				//return Activator.CreateInstance(clrType, new object[]{ value });
             }
             if (clrType == typeof(TimeSpan))
@@ -451,7 +451,7 @@ namespace SQLite.Net
             if (interfaces.Contains(typeof(ISerializable<TimeSpan>)))
             {
                 var value = new TimeSpan(_sqlitePlatform.SQLiteApi.ColumnInt64(stmt, index));
-				return this._conn.Resolver.Create(clrType, new object[] { value });
+				return this._conn.Resolver.CreateObject(clrType, new object[] { value });
 				//return Activator.CreateInstance(clrType, new object[]{ value });
             }
             if (clrType == typeof(DateTime))
@@ -490,7 +490,7 @@ namespace SQLite.Net
             if (interfaces.Contains(typeof(ISerializable<Int64>)))
             {
                 var value = _sqlitePlatform.SQLiteApi.ColumnInt64(stmt, index);
-				return this._conn.Resolver.Create(clrType, new object[] { value });
+				return this._conn.Resolver.CreateObject(clrType, new object[] { value });
 				//return Activator.CreateInstance(clrType, new object[]{ value });
             }
             if (clrType == typeof(UInt32))
@@ -500,7 +500,7 @@ namespace SQLite.Net
             if (interfaces.Contains(typeof(ISerializable<Int64>)))
             {
                 var value = (uint)_sqlitePlatform.SQLiteApi.ColumnInt64(stmt, index);
-				return this._conn.Resolver.Create(clrType, new object[] { value });
+				return this._conn.Resolver.CreateObject(clrType, new object[] { value });
 				//return Activator.CreateInstance(clrType, new object[]{ value });
             }
             if (clrType == typeof(decimal))
@@ -510,7 +510,7 @@ namespace SQLite.Net
             if (interfaces.Contains(typeof(ISerializable<decimal>)))
             {
                 var value = (decimal)_sqlitePlatform.SQLiteApi.ColumnDouble(stmt, index);
-				return this._conn.Resolver.Create(clrType, new object[] { value });
+				return this._conn.Resolver.CreateObject(clrType, new object[] { value });
 				//return Activator.CreateInstance(clrType, new object[]{ value });
             }
             if (clrType == typeof(Byte))
@@ -520,7 +520,7 @@ namespace SQLite.Net
             if (interfaces.Contains(typeof(ISerializable<Byte>)))
             {
                 var value = (byte)_sqlitePlatform.SQLiteApi.ColumnInt(stmt, index);
-				return this._conn.Resolver.Create(clrType, new object[] { value });
+				return this._conn.Resolver.CreateObject(clrType, new object[] { value });
 				//return Activator.CreateInstance(clrType, new object[]{ value });
             }
             if (clrType == typeof(UInt16))
@@ -530,7 +530,7 @@ namespace SQLite.Net
             if (interfaces.Contains(typeof(ISerializable<UInt16>)))
             {
                 var value = (ushort)_sqlitePlatform.SQLiteApi.ColumnInt(stmt, index);
-				return this._conn.Resolver.Create(clrType, new object[] { value });
+				return this._conn.Resolver.CreateObject(clrType, new object[] { value });
 				//return Activator.CreateInstance(clrType, new object[]{ value });
             }
             if (clrType == typeof(Int16))
@@ -540,7 +540,7 @@ namespace SQLite.Net
             if (interfaces.Contains(typeof(ISerializable<Int16>)))
             {
                 var value = (short)_sqlitePlatform.SQLiteApi.ColumnInt(stmt, index);
-				return this._conn.Resolver.Create(clrType, new object[] { value });
+				return this._conn.Resolver.CreateObject(clrType, new object[] { value });
 				//return Activator.CreateInstance(clrType, new object[]{ value });
             }
             if (clrType == typeof(sbyte))
@@ -550,7 +550,7 @@ namespace SQLite.Net
             if (interfaces.Contains(typeof(ISerializable<sbyte>)))
             {
                 var value = (sbyte)_sqlitePlatform.SQLiteApi.ColumnInt(stmt, index);
-				return this._conn.Resolver.Create(clrType, new object[] { value });
+				return this._conn.Resolver.CreateObject(clrType, new object[] { value });
 				//return Activator.CreateInstance(clrType, new object[]{ value });
 			}
 			if (clrType == typeof(byte[]))
@@ -560,7 +560,7 @@ namespace SQLite.Net
             if (interfaces.Contains(typeof(ISerializable<byte[]>)))
             {
                 var value = _sqlitePlatform.SQLiteApi.ColumnByteArray(stmt, index);
-				return this._conn.Resolver.Create(clrType, new object[] { value });
+				return this._conn.Resolver.CreateObject(clrType, new object[] { value });
 				//return Activator.CreateInstance(clrType, new object[]{ value });
 			}
 			if (clrType == typeof(Guid))
@@ -570,7 +570,7 @@ namespace SQLite.Net
             if (interfaces.Contains(typeof(ISerializable<Guid>)))
             {
                 var value = new Guid(_sqlitePlatform.SQLiteApi.ColumnText16(stmt, index));
-				return this._conn.Resolver.Create(clrType, new object[] { value });
+				return this._conn.Resolver.CreateObject(clrType, new object[] { value });
 				//return Activator.CreateInstance(clrType, new object[]{ value });
 			}
 			if (_conn.Serializer != null && _conn.Serializer.CanDeserialize(clrType))
