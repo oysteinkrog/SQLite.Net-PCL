bc91dfda6d084f2b770741ed5cf3d31d4a9ac90f
c5185595 <s.kiefer@sap.com>
2015-06-18 12:50:15 +0200

    cleanup

--- tests/JoinTests.cs
+++ tests/JoinTests.cs
@@ -1,36 +1,18 @@
-﻿using System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using SQLite.Net.Attributes;
 using SQLite.Net.Interop;
 using System.Linq;
 
 #if __WIN32__
 using SQLitePlatformTest = SQLite.Net.Platform.Win32.SQLitePlatformWin32;
-
 #elif WINDOWS_PHONE
 using SQLitePlatformTest = SQLite.Net.Platform.WindowsPhone8.SQLitePlatformWP8;
-
-
-
-
 #elif __WINRT__
 using SQLitePlatformTest = SQLite.Net.Platform.WinRT.SQLitePlatformWinRT;
-
-
-
-
 #elif __IOS__
 using SQLitePlatformTest = SQLite.Net.Platform.XamarinIOS.SQLitePlatformIOS;
-
-
-
-
 #elif __ANDROID__
 using SQLitePlatformTest = SQLite.Net.Platform.XamarinAndroid.SQLitePlatformAndroid;
-
-
-
-
 #else
 using SQLitePlatformTest = SQLite.Net.Platform.Generic.SQLitePlatformGeneric;
 #endif
