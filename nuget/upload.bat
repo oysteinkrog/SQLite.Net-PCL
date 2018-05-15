1247e7d2b798f30a42b39a3a83920bd2851be08d
Michael Fink <mfi@bittner-krull.de>
2018-05-15 15:28:57 +0200

    updated version number from 3.1.1 to 4.0.0-beta1

--- nuget/upload.bat
+++ nuget/upload.bat
@@ -1,3 +1,3 @@
-..\.nuget\nuget push output\SQLite.Net.Core-PCL.3.1.1.nupkg
-..\.nuget\nuget push output\SQLite.Net-PCL.3.1.1.nupkg
-..\.nuget\nuget push output\SQLite.Net.Async-PCL.3.1.1.nupkg
+..\.nuget\nuget push output\SQLite.Net.Core-PCL.4.0.0-beta1.nupkg
+..\.nuget\nuget push output\SQLite.Net-PCL.4.0.0-beta1.nupkg
+..\.nuget\nuget push output\SQLite.Net.Async-PCL.4.0.0-beta1.nupkg
